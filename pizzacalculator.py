5cf7fbeb8c5b8a5cb94116e6cdfe70ed91581196
Straatmongol <99067444@mydavinci.nl>
2021-09-09 15:28:13 +0200

    Eerste commit!

---
+++ pizzacalculator.py
@@ -0,0 +1,11 @@
+# Nabil Laiti 99067444
+
+prijssmall = 5.25
+prijsmedium = 7.76
+prijslarge = 10.99
+
+small = int(input(f"Hoeveel small pizza's wilt u? (€{prijssmall}) : "))
+medium = int(input(f"Hoeveel medium pizza's wilt? (€{prijsmedium}) : ")) 
+large = int(input(f"Hoeveel large pizza's wilt u? (€{prijslarge}) : "))
+
+print(f' Je bestelling is in totaal €{round((prijssmall * small) + (prijsmedium * medium) + (prijslarge * large), 2)}, Kies u betaalmethode.') 
