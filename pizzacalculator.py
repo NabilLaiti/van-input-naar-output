0215252d992a6876928c478bec310bc7c24e0d8b
Straatmongol <99067444@mydavinci.nl>
2021-09-22 13:25:30 +0200

    Eerste commit!

--- pizzacalculator.py
+++ pizzacalculator.py
@@ -9,4 +9,10 @@ small = int(input(f"Hoeveel small pizza's wilt u? (€{prijssmall}) : "))
 medium = int(input(f"Hoeveel medium pizza's wilt? (€{prijsmedium}) : ")) 
 large = int(input(f"Hoeveel large pizza's wilt u? (€{prijslarge}) : "))
 
+print("--------------------------------------------------------------------------")
+
+print(f"{small}x small pizza's : (€{prijssmall*small}")
+print(f"{medium}x medium pizza's : (€{prijsmedium*medium}")
+print(f"{large}x large pizza's : (€{prijslarge*large}")
+
 print(f' Je bestelling is in totaal €{round((prijssmall * small) + (prijsmedium * medium) + (prijslarge * large), 2)}, Kies u betaalmethode.') 
